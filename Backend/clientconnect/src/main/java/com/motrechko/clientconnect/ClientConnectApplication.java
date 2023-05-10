edd80f4d13e8786d00ec60e4d3c62f555eaaa66f
volodymyrm27 <vovam6655@gmail.com>
2023-05-10 17:32:18 +0300

    initial commit - Added user - Added UserRepository - Added roles - Added implements UserDetails interface in User.java - Configurate application.yml

--- Backend/clientconnect/src/main/java/com/motrechko/clientconnect/ClientConnectApplication.java
+++ Backend/clientconnect/src/main/java/com/motrechko/clientconnect/ClientConnectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientconnectApplication {
+public class ClientConnectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientconnectApplication.class, args);
+		SpringApplication.run(ClientConnectApplication.class, args);
 	}
 
 }
---
+++ Backend/clientconnect/src/main/java/com/motrechko/clientconnect/user/Role.java
@@ -0,0 +1,6 @@
+package com.motrechko.clientconnect.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}

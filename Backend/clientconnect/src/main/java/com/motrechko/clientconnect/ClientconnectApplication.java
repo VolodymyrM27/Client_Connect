3ea60092191ec40952b8e69dac0201dc3251c77f
volodymyrm27 <vovam6655@gmail.com>
2023-05-10 16:28:53 +0300

    Initial commit

---
+++ Backend/clientconnect/src/main/java/com/motrechko/clientconnect/ClientconnectApplication.java
@@ -0,0 +1,13 @@
+package com.motrechko.clientconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientconnectApplication.class, args);
+	}
+
+}
---
+++ Backend/clientconnect/src/test/java/com/motrechko/clientconnect/ClientconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motrechko.clientconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
